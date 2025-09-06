cb0e3da7eb4db9939345262b8c83286d8940630b
Joe <tan.eu.joe@gmail.com>
2025-09-06 13:02:45 +0800

    added package for hashing pw, User and Usercontroller file

---
+++ backend/src/main/java/com/tariff/app/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.tariff.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/login")
+@CrossOrigin(origins="*")
+public class UserController {
+    
+    // @PostMapping("/login")
+    // public ResponseEntity<UserLoginResponse> login(@RequestBody  ){
+
+    // }
+
+    // @PostMapping("/signup")
+    // public ResponseEntity<UserSignupResponse> signup(@RequestBody){
+
+    // }
+    
+}
