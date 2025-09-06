3df441d51f947466f8969befdc96e1b45bb16a2a
Joe <tan.eu.joe@gmail.com>
2025-09-06 14:02:41 +0800

    User sign up

--- backend/src/main/java/com/tariff/app/service/UserService.java
+++ backend/src/main/java/com/tariff/app/service/UserService.java
@@ -7,30 +7,48 @@ import org.springframework.stereotype.Service;
 
 import com.tariff.app.dto.UserLoginRequest;
 import com.tariff.app.dto.UserLoginResponse;
+import com.tariff.app.dto.UserSignupRequest;
+import com.tariff.app.dto.UserSignupResponse;
 import com.tariff.app.entity.User;
 import com.tariff.app.repository.UserRepository;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository userRepository;
 
-    public UserLoginResponse loginUser(UserLoginRequest request){
+    public UserLoginResponse loginUser(UserLoginRequest request) {
         Optional<User> userOptional = userRepository.findByUsername(request.getUsername());
-        if (userOptional.isPresent()){
+        if (userOptional.isPresent()) {
             String rawPassword = request.getPassword();
             User user = userOptional.get();
-            if (user.verifyPassword(rawPassword)){
+            if (user.verifyPassword(rawPassword)) {
                 // Successful Authentication
                 return new UserLoginResponse(
-
-                );
+                        "you have succeeded");
             }
         }
         return new UserLoginResponse(
-            request.getUsername()
-        );
+                "you have failed");
+    }
+
+    public UserSignupResponse signupUser(UserSignupRequest request) {
+        Optional<User> userOptional = userRepository.findByUsername(request.getUsername());
+        if (userOptional.isPresent()) {
+            // user already exists, cannot sign up
+            return new UserSignupResponse(
+                    "User already exists");
+        } else {
+            User user = new User();
+            user.setUsername(request.getUsername());
+            user.setHashPassword(request.getPassword());
+
+            // supposed to save the user to db
+            userRepository.save(user);
+            return new UserSignupResponse(
+                    "Successful");
+        }
     }
 
 }
