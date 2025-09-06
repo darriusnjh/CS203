0f6b8dbeb1134a7ddd39bef7dcdf60422452dac4
Joe <tan.eu.joe@gmail.com>
2025-09-06 13:02:45 +0800

    added package for hashing pw, User and Usercontroller file

--- backend/src/main/java/com/tariff/app/entity/User.java
+++ backend/src/main/java/com/tariff/app/entity/User.java
@@ -1,6 +1,7 @@
 package com.tariff.app.entity;
 
 import javax.persistence.*;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @Entity
 @Table(name = "users")
@@ -8,41 +9,52 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    @Column(name="id")
+    @Column(name = "id")
     private Long id;
 
-    @Column(name="username")
-    private String username; 
+    @Column(name = "username")
+    private String username;
 
-    @Column(name="password")
+    @Column(name = "password")
     private String password;
 
+    private static final Argon2PasswordEncoder encoder = new Argon2PasswordEncoder();
+
     // Default constructor
-    public User(){
+    public User() {
 
     }
-    
+
     // Constructor with parameters
-    public User(Long id, String username, String password){
-        this.id = id; 
+    public User(Long id, String username, String password) {
+        this.id = id;
         this.username = username;
         this.password = password;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username){
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password){
+    public void setPassword(String password) {
         this.password = password;
     }
-    
+
+    public void setHashPassword(String password) {
+        String hashedPassword = encoder.encode(password);
+        this.password = hashedPassword;
+    }
+
+    public boolean verifyPassword(String rawPassword, String hashedPassword){
+        return encoder.matches(rawPassword, hashedPassword);
+    }
+
 }
