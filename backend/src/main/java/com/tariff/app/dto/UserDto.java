857865c4f8b829397288de61ec5ba31539e30ccd
Anson Koh <ansonkoh19@gmail.com>
2025-09-22 21:30:22 +0800

    style: Reorder imports in UserDto for better organization
    - Move Java standard library imports to top
    - Group Jackson annotations together
    - Improve code readability and maintainability

--- backend/src/main/java/com/tariff/app/dto/UserDto.java
+++ backend/src/main/java/com/tariff/app/dto/UserDto.java
@@ -1,14 +1,15 @@
 package com.tariff.app.dto;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.OffsetDateTime;
-import java.util.UUID;
 
 
 @AllArgsConstructor
